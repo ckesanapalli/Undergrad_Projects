5b96afc976feb91bd3cefd8cd52d7a29108f987a
Chaitanya Kesanapalli <ckesanapalli@gmail.com>
2020-02-21 19:35:40 -0600

    Create test.py

---
+++ Web_Ops_Projects/windows-app-15/test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""<Description of the program file>
+"""
+# =============================================================================
+# <one line to give the program's name and a brief idea of what it does.>
+# Copyright (C) 2020 Chaitanya Chowdary Kesanapalli
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+# Modified by Chaitanya Chowdary Kesanapalli, 2020.
+# =============================================================================
+
+from __future__ import absolute_import, division, print_function
+# from builtins import *
+#from glob import glob
+#from pprint import pprint
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import pandas as pd
+#import os
+
+__author__ = "Chaitanya Chowdary Kesanapalli"
+__copyright__ = "Copyright 2020, The <Project Name> Project"
+__credits__ = [__author__]
+__license__ = "GPL"
+__version__ = "3.0"
+__maintainer__ = "Chaitanya Chowdary Kesanapalli"
+__email__ = "ckesanapalli@gmail.com"
+__status__ = "Production"
+
+# Add classes and functions in __all__
+__all__ = [
+    
+    ]
+
+
+# =============================================================================
+# Classes and Functions
+# =============================================================================
+
+
+
+# =============================================================================
+# Main Program
+# =============================================================================
+def main():
+    """This is the main function"""
+
+if __name__ == '__main__':
+    main()
